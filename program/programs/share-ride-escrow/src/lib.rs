785a8806b039171e547c82964b214518fb40899e
shamin.meerankutty <shamin616@gmail.com>
2021-08-29 23:16:13 +0530

    program: Intialising an escrow account

--- program/programs/share-ride-escrow/src/lib.rs
+++ program/programs/share-ride-escrow/src/lib.rs
@@ -1,60 +1,69 @@
-// #region code
 use anchor_lang::prelude::*;
-use borsh::{BorshDeserialize, BorshSerialize};
+use anchor_spl::token::{self, SetAuthority, TokenAccount, Transfer};
+use spl_token::instruction::AuthorityType;
 
 #[program]
 pub mod share_ride_escrow {
     use super::*;
 
-    #[state]
-    pub struct ShareRideState {
-        pub authority: Pubkey,
-        pub drivers: Vec<Driver>,
-        pub index: i32,
-    }
+    pub fn initialize_escrow(
+        ctx: Context<InitializeEscrow>,
+        passenger_amount: u64
+    ) -> ProgramResult {
+        ctx.accounts.escrow_account.passenger_key = *ctx.accounts.passenger.key;
+        ctx.accounts
+            .escrow_account
+            .passenger_deposit_token_account = *ctx
+            .accounts
+            .passenger_deposit_token_account
+            .to_account_info()
+            .key;
+        ctx.accounts.escrow_account.passenger_amount = passenger_amount;
+        ctx.accounts.escrow_account.driver_key = *ctx.accounts.driver.key;
 
-    impl ShareRideState {
-        pub const DRIVER_SIZE: usize = 10;
-        pub fn new(ctx: Context<Auth>) -> Result<Self> {
-            let mut drivers = vec![];
-            drivers.resize(Self::DRIVER_SIZE, Driver { address: String::from("123456789"), location: String::from("123456789"), seat: String::from("123456789")});
-            Ok(Self {
-                authority: *ctx.accounts.authority.key,
-                drivers,
-                index: 0,
-            })
-        }
-
-        pub fn add_driver(&mut self, ctx: Context<Auth>, driver: Driver) -> Result<()> {
-            if &self.authority != ctx.accounts.authority.key {
-                return Err(ErrorCode::Unauthorized.into());
-            }
-            msg!("New Driver {:?}", driver);
-            msg!("Existing Drivers {:?}", self.drivers);
-            let num_usize: usize = self.index as usize;
-            msg!("U Size {:?}", num_usize);
-            self.drivers[num_usize] = driver;
-            self.index += 1;
-            Ok(())
-        }
+        let (pda, _bump_seed) = Pubkey::find_program_address(&[b"escrow"], ctx.program_id);
+        token::set_authority(ctx.accounts.into(), AuthorityType::AccountOwner, Some(pda))?;
+        Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Auth<'info> {
+#[instruction(passenger_amount: u64)]
+pub struct InitializeEscrow<'info> {
     #[account(signer)]
-    authority: AccountInfo<'info>,
+    pub passenger: AccountInfo<'info>,
+    pub driver: AccountInfo<'info>,
+    #[account(
+        mut,
+        constraint = passenger_deposit_token_account.amount >= passenger_amount
+    )]
+    pub passenger_deposit_token_account: CpiAccount<'info, TokenAccount>,
+    #[account(init)]
+    pub escrow_account: ProgramAccount<'info, EscrowAccount>,
+    pub token_program: AccountInfo<'info>,
 }
 
-#[derive(AnchorSerialize, AnchorDeserialize, Default, Clone, Debug)]
-pub struct Driver {
-    address: String,
-    location: String,
-    seat: String,
+
+#[account]
+pub struct EscrowAccount {
+    pub passenger_key: Pubkey,
+    pub passenger_deposit_token_account: Pubkey,
+    pub passenger_amount: u64,
+    pub driver_key: Pubkey,
 }
 
-#[error]
-pub enum ErrorCode {
-    #[msg("You are not authorized to perform this action.")]
-    Unauthorized,
+impl<'info> From<&mut InitializeEscrow<'info>>
+    for CpiContext<'_, '_, '_, 'info, SetAuthority<'info>>
+{
+    fn from(accounts: &mut InitializeEscrow<'info>) -> Self {
+        let cpi_accounts = SetAuthority {
+            account_or_mint: accounts
+                .passenger_deposit_token_account
+                .to_account_info()
+                .clone(),
+            current_authority: accounts.passenger.clone(),
+        };
+        let cpi_program = accounts.token_program.clone();
+        CpiContext::new(cpi_program, cpi_accounts)
+    }
 }
--- program/tests/share-ride-escrow.js
+++ program/tests/share-ride-escrow.js
@@ -1,13 +1,6 @@
-const assert = require("assert");
 const anchor = require("@project-serum/anchor");
-
-class Driver {
-  constructor(properties) {
-    Object.keys(properties).map((key) => {
-      this[key] = properties[key];
-    });
-  }
-}
+const { TOKEN_PROGRAM_ID, Token } = require("@solana/spl-token");
+const assert = require("assert");
 
 describe("escrow", () => {
   const provider = anchor.Provider.local();
@@ -16,29 +9,82 @@ describe("escrow", () => {
 
   const program = anchor.workspace.ShareRideEscrow;
 
-  it("Is runs the constructor", async () => {
-    await program.state.rpc.new({
-      accounts: {
-        authority: provider.wallet.publicKey,
-      },
-    });
-    const state = await program.state.fetch();
-    assert.ok(state.drivers.length === 10);
+  let mintA = null;
+  let passengerTokenAccountA = null;
+  const passengerAmount = 20;
+
+  const escrowAccount = anchor.web3.Keypair.generate();
+  const payer = anchor.web3.Keypair.generate();
+  const mintAuthority = anchor.web3.Keypair.generate();
+  
+  const driver = anchor.web3.Keypair.generate();
+
+  it("Initialise escrow state", async () => {
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(payer.publicKey, 10000000000),
+      "confirmed"
+    );
+
+    mintA = await Token.createMint(
+      provider.connection,
+      payer,
+      mintAuthority.publicKey,
+      null,
+      0,
+      TOKEN_PROGRAM_ID
+    );
+
+    passengerTokenAccountA = await mintA.createAccount(
+      provider.wallet.publicKey
+    );
+
+    await mintA.mintTo(
+      passengerTokenAccountA,
+      mintAuthority.publicKey,
+      [mintAuthority],
+      passengerAmount
+    );
+
+    let _passengerTokenAccountA = await mintA.getAccountInfo(
+      passengerTokenAccountA
+    );
+
+    assert.ok(_passengerTokenAccountA.amount.toNumber() == passengerAmount);
   });
 
-  const value = new Driver({
-    address: "asdfrghld",
-    location: "qwsderefd",
-    seat: "zxcdserdf",
-  });
-
-  it("Executes a method on the program", async () => {
-    await program.state.rpc.addDriver(value, {
+  it("Intialize escrow", async () => {
+    await program.rpc.initializeEscrow(new anchor.BN(passengerAmount), {
       accounts: {
-        authority: provider.wallet.publicKey,
+        passenger: provider.wallet.publicKey,
+        passengerDepositTokenAccount: passengerTokenAccountA,
+        escrowAccount: escrowAccount.publicKey,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+        driver: driver.publicKey,
       },
+      instructions: [
+        await program.account.escrowAccount.createInstruction(escrowAccount),
+      ],
+      signers: [escrowAccount],
     });
-    const state = await program.state.fetch();
-    assert.ok(state.drivers.length === 10);
+
+    const [pda, _nonce] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from(anchor.utils.bytes.utf8.encode("escrow"))],
+      program.programId
+    );
+
+    let _passengerTokenAccountA = await mintA.getAccountInfo(passengerTokenAccountA);
+    let _escrowAccount = await program.account.escrowAccount.fetch(
+      escrowAccount.publicKey
+    );
+  
+    assert.ok(_passengerTokenAccountA.owner.equals(pda));
+    assert.ok(_escrowAccount.passengerKey.equals(provider.wallet.publicKey));
+    assert.ok(_escrowAccount.driverKey.equals(driver.publicKey));
+
+    assert.ok(_escrowAccount.passengerAmount.toNumber() == passengerAmount);
+    assert.ok(
+      _escrowAccount.passengerDepositTokenAccount.equals(passengerTokenAccountA)
+    );
   });
 });
