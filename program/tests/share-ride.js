27ccac39753ed32f89c541897e7c89662ca7bcc1
shamin.meerankutty <shamin616@gmail.com>
2021-08-29 21:06:56 +0530

    program: Share ride basic

--- program/tests/share-ride.js
+++ program/tests/share-ride.js
@@ -1,12 +1,44 @@
+const assert = require("assert");
 const anchor = require("@project-serum/anchor");
+
+class Driver {
+  constructor(properties) {
+    Object.keys(properties).map((key) => {
+      this[key] = properties[key];
+    });
+  }
+}
 
 describe("share-ride", () => {
   const provider = anchor.Provider.local();
+
   anchor.setProvider(provider);
 
   const program = anchor.workspace.ShareRide;
 
   it("Is runs the constructor", async () => {
-    await program.rpc.initialize();
+    await program.state.rpc.new({
+      accounts: {
+        authority: provider.wallet.publicKey,
+      },
+    });
+    const state = await program.state.fetch();
+    assert.ok(state.drivers.length === 10);
+  });
+
+  const value = new Driver({
+    address: "asdfrghld",
+    location: "qwsderefd",
+    seat: "zxcdserdf",
+  });
+
+  it("Executes a method on the program", async () => {
+    await program.state.rpc.addDriver(value, {
+      accounts: {
+        authority: provider.wallet.publicKey,
+      },
+    });
+    const state = await program.state.fetch();
+    assert.ok(state.drivers.length === 10);
   });
 });
